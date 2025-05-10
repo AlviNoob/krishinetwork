2228249c84316d240831a29160c840f974e24da3
Alvi Sakib Orin <106269664+AlviNoob@users.noreply.github.com>
2025-05-10 23:41:59 +0600

    Delete server-20250510T163026Z-1-001 directory

--- server-20250510T163026Z-1-001/server/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-  author: String,
-  role: String,
-  content: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Post', postSchema);
